168a236c251fe92d86029c33fc53ac160cdb8c6f
Pascal Holy <pascal@airy.co>
2020-12-09 20:15:53 +0100

    Add go lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,3 +7,11 @@ bazel run @com_github_airyhq_bazel_tools//code-format:check_buildifier
 echo
 echo "Running Prettier and Java tests"
 bazel test --test_tag_filters=lint //...
+
+go get -u golang.org/x/lint/golint
+
+if [[ $(golint ./...) ]]; then
+    exit 0
+else
+    exit 1
+fi
